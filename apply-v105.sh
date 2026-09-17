6aa742f0e35696fd737631f8f171d8ce4a4f01bf
Meetup App Dev <dev@meetup-app.local>
2026-09-17 10:37:51 +0500

    fix: значок своего предложения на карте

--- apply-v105.sh
+++ apply-v105.sh
@@ -1,3 +1,4 @@
+cat > "components/map/EventsMap.tsx" << 'ENDOFFILE'
 "use client";
 
 import { forwardRef, useEffect, useImperativeHandle, useRef } from "react";
@@ -296,3 +297,4 @@ export const EventsMap = forwardRef<EventsMapHandle, EventsMapProps>(function Ev
 
   return <div ref={containerRef} className="h-full w-full" />;
 });
+ENDOFFILE
