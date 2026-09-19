0ead9886f041d4bec041f9ce0419cee6dbdd3ef4
Meetup App Dev <dev@meetup-app.local>
2026-09-19 12:05:43 +0500

    feat: кнопка Назад на первом шаге

--- apply-v135.sh
+++ apply-v135.sh
@@ -1,3 +1,4 @@
+cat > "components/create-event/CreateEventWizard.tsx" << 'ENDOFFILE'
 "use client";
 
 import Image from "next/image";
@@ -725,3 +726,4 @@ function ReviewRow({ label, value }: { label: string; value: string }) {
     </div>
   );
 }
+ENDOFFILE
